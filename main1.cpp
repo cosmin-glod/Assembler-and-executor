a34b93a5a0f0d06b3efe74e768c0d459d58f160e
Andu9 <153365754+Andu9@users.noreply.github.com>
2024-01-13 22:37:50 +0200

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <bitset>
+#include <vector>
 #include <string>
 #include <map>
 
@@ -12,7 +13,10 @@ struct componenetsRegisters {
     int val;
     bool isInt = false;
 
-    std::string String;
+    char car;
+    bool isChar = false;
+
+    std::vector<char> String;
     bool isString = false;
 };
 
@@ -31,17 +35,23 @@ int main () {
 
         i += 1;
 
-
         std::string dataType = "";
-        while (line[i] != NULL && line[i] != ' ')
+        while (line[i] != ' ')
             dataType += line[i], i += 1;
 
         i += 1;
 
         line.erase (0, i);
 
+        std::vector<char> value;
+
+        for (int i = 0; i < line.size (); i += 1)
+            value.push_back (line[i]);
+
+        value.push_back (0);
+
         if (dataType == "string") {
-            registers[currentRegister].String = line;
+            registers[currentRegister].String = value;
             registers[currentRegister].isString = true;
         }
         //line.erase (0, i);
@@ -75,6 +85,7 @@ int main () {
     codeRegisters[81] = "fa1";
     codeRegisters[82] = "fa2";
 
+
 /*
     cod_functions[112] = "strlen";
     cod_functions[96] = "printf";
@@ -82,27 +93,44 @@ int main () {
     cod_functions[113] = "cfunc";
 */
 
-    //std::cout << std::hex;
-
     char code;
     executabil.read (&code, 1);
     while (code != 66) {
-        if (code == 4) {
+        if (code == 4) { /// li
             char currentRegister;
             executabil.read (&currentRegister, 1);
 
             int constant;
             executabil.read (reinterpret_cast<char *> (&constant), 4);
-        } else if (code == 16) {
+
+            registers[codeRegisters[currentRegister]].isInt = true;
+            registers[codeRegisters[currentRegister]].val = constant;
+       } else if (code == 16) { /// add
             char currentRegister1;
             executabil.read (&currentRegister1, 1);
+            int i = currentRegister1;
 
             char currentRegister2;
             executabil.read (&currentRegister2, 1);
+            int j = currentRegister2;
 
             char currentRegister3;
             executabil.read (&currentRegister3, 1);
-        } else if (code == 1) {
+            int k = currentRegister3;
+
+
+            if (registers[codeRegisters[j]].isString == true) {
+                registers[codeRegisters[i]].isString = true;
+                std::vector<char> v(registers[codeRegisters[j]].String);
+                std::vector<char> res(v.begin () + registers[codeRegisters[k]].val, v.end ());
+                registers[codeRegisters[i]].String = res;
+            } else if (registers[codeRegisters[k]].isString == true) {
+                registers[codeRegisters[i]].isString = true;
+                std::vector<char> v(registers[codeRegisters[k]].String);
+                std::vector<char> res(v.begin () + registers[codeRegisters[j]].val, v.end ());
+                registers[codeRegisters[i]].String = res;
+            }
+        } else if (code == 1) { /// lb
             char currentRegister1;
             executabil.read (&currentRegister1, 1);
 
@@ -111,13 +139,21 @@ int main () {
 
             char currentRegister2;
             executabil.read (&currentRegister2, 1);
-        } else if (code == 80) {
+
+            if (registers[codeRegisters[currentRegister2]].isString == true) {
+                registers[codeRegisters[currentRegister1]].isChar = true;
+                registers[codeRegisters[currentRegister1]].car = registers[codeRegisters[currentRegister2]].String[constant];
+            }
+        } else if (code == 80) { /// beqz
             char currentRegister;
             executabil.read (&currentRegister, 1);
 
-            int constant;
+            short constant;
             executabil.read (reinterpret_cast<char *> (&constant), 2);
-        } else if (code == 17) {
+
+            if ((int) registers[codeRegisters[currentRegister]].car == 0)
+                executabil.seekp (constant, std::ios_base::beg);
+        } else if (code == 17) { /// addi
             char currentRegister1;
             executabil.read (&currentRegister1, 1);
 
@@ -126,17 +162,34 @@ int main () {
 
             int constant;
             executabil.read (reinterpret_cast<char *> (&constant), 4);
-        } else if (code == 64) {
-            int constant;
+
+
+            registers[codeRegisters[currentRegister1]].isInt = true;
+            registers[codeRegisters[currentRegister1]].val = registers[codeRegisters[currentRegister2]].val + constant;
+        } else if (code == 64) { /// j
+            short constant;
             executabil.read (reinterpret_cast<char *> (&constant), 2);
-        } else if (code == 34) {
+
+            executabil.seekp (constant, std::ios_base::beg);
+        } else if (code == 34) { /// mv
             char currentRegister1;
             executabil.read (&currentRegister1, 1);
 
             char currentRegister2;
             executabil.read (&currentRegister2, 1);
+
+
+            if (registers[codeRegisters[currentRegister2]].isString == true) {
+                registers[codeRegisters[currentRegister1]].isString = true;
+                registers[codeRegisters[currentRegister1]].String = registers[codeRegisters[currentRegister2]].String;
+            } else if (registers[codeRegisters[currentRegister2]].isInt == true) {
+                registers[codeRegisters[currentRegister1]].isInt = true;
+                registers[codeRegisters[currentRegister1]].val = registers[codeRegisters[currentRegister2]].val;
+            }
         }
         executabil.read (&code, 1);
     }
+
+    std::cout << registers["a0"].val;
     return 0;
 }
